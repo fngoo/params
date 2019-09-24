55b09b0db7e9699bbd09930b3d0e2403ce65f968
fngoo <38164287+fngoo@users.noreply.github.com>
2019-09-25 02:47:11 +0800

    Update params.sh

--- params.sh
+++ params.sh
@@ -16,7 +16,7 @@ echo "cd /root/script/3_httprobe/dir_$i" >> /root/script/3_httprobe/dir_$i/${i}.
 echo "cp -r /root/script/3_httprobe/Arjun/* /root/script/3_httprobe/dir_$i" >> /root/script/3_httprobe/dir_$i/${i}.sh
 echo "python3 arjun.py -u \"$line\" -t 2 -d 0 -o 1.txt --get" >> /root/script/3_httprobe/dir_$i/${i}.sh
 echo 'grep -oP "param\"\:\ \".*" 1.txt | grep -oP "\ \".*\"" | sed -e "s/\"//g" | sed -e "s/\ //g" > 2.txt' >> /root/script/3_httprobe/dir_$i/${i}.sh
-echo 'for param in `cat 2.txt`; do echo "$line?${param}=1" >> /root/script/3_httprobe/params_xss_test.txt; done' >> /root/script/3_httprobe/dir_$i/${i}.sh
+echo 'for param in `cat 2.txt`; do echo "${line}?${param}=1" >> /root/script/3_httprobe/params_xss_test.txt; done' >> /root/script/3_httprobe/dir_$i/${i}.sh
 echo "rm -r /root/script/3_httprobe/dir_$i" >> /root/script/3_httprobe/dir_$i/${i}.sh
 echo "bash /root/script/3_httprobe/dir_$i/${i}.sh" >> /root/script/3_httprobe/exe.sh
 i=$((i+1))
