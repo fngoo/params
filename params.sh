208185262bd77d5680382b117e64cb04deafe4f5
fngoo <38164287+fngoo@users.noreply.github.com>
2019-10-23 08:40:51 +0800

    Update params.sh

--- params.sh
+++ params.sh
@@ -23,7 +23,7 @@ echo "bash /root/script/3_httprobe/dir_$i/${i}.sh" >> /root/script/3_httprobe/ex
 i=$((i+1))
 
 done
-cat /root/script/3_httprobe/exe.sh | parallel --jobs 0 --delay 0.6 --retries 1 --timeout 360
+cat /root/script/3_httprobe/exe.sh | parallel --jobs 0 --delay 0.6 --retries 1 --timeout 60
 rm /root/script/3_httprobe/exe.sh
 rm /root/script/3_httprobe/dir_* -r
 
@@ -70,7 +70,7 @@ echo "bash /root/script/3_httprobe/dir_$i/${i}.sh" >> /root/script/3_httprobe/ex
 i=$((i+1))
 
 done
-cat /root/script/3_httprobe/exe.sh | parallel --jobs 0 --delay 0.6 --retries 1 --timeout 360
+cat /root/script/3_httprobe/exe.sh | parallel --jobs 0 --delay 0.6 --retries 1 --timeout 60
 rm /root/script/3_httprobe/exe.sh
 rm dir_* -r
 
